2e13f8fcb28fb9561e86e52cc87e232e94e2d1ab
amir-sabzi-417 <97.amirsabzi@gmail.com>
2022-02-12 00:09:26 +0000

    script works fine with eno1, now changed for enp66s0f1

--- codes/socket_code/socket_timestamping.c
+++ codes/socket_code/socket_timestamping.c
@@ -48,10 +48,10 @@ static int setup_udp_receiver(socket_info *inf, int port) {
   hw_config.tx_type = HWTSTAMP_TX_ON;
 
   // Timestamping filter: (CAUTION: should be available as an option for the NIC)
-   hw_config.rx_filter = HWTSTAMP_FILTER_PTP_V2_L2_EVENT;
+   hw_config.rx_filter = HWTSTAMP_FILTER_PTP_V2_EVENT;
   
   // Interface Name: (CAUTION: consider changing the rx_filter when you change Interface)
-  char* interface_name = "eno1";
+  char* interface_name = "enp66s0f1";
   
   struct ifreq hwtstamp;
   memset(&hwtstamp, 0, sizeof(hwtstamp));
@@ -138,10 +138,10 @@ static int setup_udp_sender(socket_info *inf, int port, char *address) {
   hw_config.tx_type = HWTSTAMP_TX_ON;
 
   // Timestamping filter: (CAUTION: should be available as an option for the NIC)
-   hw_config.rx_filter = HWTSTAMP_FILTER_PTP_V2_L2_EVENT;
+   hw_config.rx_filter = HWTSTAMP_FILTER_PTP_V2_EVENT;
   
   // Interface Name: (CAUTION: consider changing the rx_filter when you change Interface)
-  char* interface_name = "eno1";
+  char* interface_name = "enp66s0f1";
   
   struct ifreq hwtstamp;
   memset(&hwtstamp, 0, sizeof(hwtstamp));
